38cda243797b12bc35af13d84a513fb4c9646b3e
Wostan <kostyantynchyk@gmail.com>
2025-10-20 22:01:46 +0300

    Created user domain events

---
+++ src/ForgeFit.Domain/Events/UserEvents/UserRegisteredEvent.cs
@@ -0,0 +1,10 @@
+﻿using ForgeFit.Domain.Primitives.Interfaces;
+using ForgeFit.Domain.ValueObjects.UserValueObjects;
+
+namespace ForgeFit.Domain.Events.UserEvents;
+
+public class UserRegisteredEvent(Guid userId, UserProfile userProfile) : IDomainEvent
+{
+    public Guid UserId { get; } = userId;
+    public UserProfile UserProfile { get; } = userProfile;
+}
