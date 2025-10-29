cc49cda588eb40f96c0231238fea6b443a110268
Wostan <kostyantynchyk@gmail.com>
2025-10-30 00:03:27 +0200

    Completed User events

--- src/ForgeFit.Domain/Events/UserEvents/UserRegisteredEvent.cs
+++ src/ForgeFit.Domain/Events/UserEvents/UserRegisteredEvent.cs
@@ -3,7 +3,7 @@ using ForgeFit.Domain.ValueObjects.UserValueObjects;
 
 namespace ForgeFit.Domain.Events.UserEvents;
 
-public class UserRegisteredEvent(Guid userId, UserProfile userProfile) : IDomainEvent
+public class UserRegisteredEvent(Guid userId, UserProfile userProfile) : DomainEvent
 {
     public Guid UserId { get; } = userId;
     public UserProfile UserProfile { get; } = userProfile;
