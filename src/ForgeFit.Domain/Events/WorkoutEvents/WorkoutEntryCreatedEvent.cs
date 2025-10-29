7672647cec65785e174576b32ef3772df47cbf14
Wostan <kostyantynchyk@gmail.com>
2025-10-30 01:25:27 +0200

    Completed workout events

---
+++ src/ForgeFit.Domain/Events/WorkoutEvents/WorkoutEntryCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using ForgeFit.Domain.Primitives.Interfaces;
+using ForgeFit.Domain.ValueObjects;
+
+namespace ForgeFit.Domain.Events.WorkoutEvents;
+
+public class WorkoutEntryCreatedEvent(
+    Guid workoutEntryId,
+    Guid workoutProgramId,
+    Guid userId,
+    Schedule workoutSchedule) : DomainEvent
+{
+    public Guid WorkoutEntryId { get; } = workoutEntryId;
+    public Guid WorkoutProgramId { get; } = workoutProgramId;
+    public Guid UserId { get; } = userId;
+    public Schedule WorkoutSchedule { get; } = workoutSchedule;
+}
