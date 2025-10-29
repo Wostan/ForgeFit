fdf17e8f33ba928b50861cdf474a02800646755b
Wostan <kostyantynchyk@gmail.com>
2025-10-30 01:12:42 +0200

    Completed nutrition events

---
+++ src/ForgeFit.Domain/Events/FoodEvents/DrinkEntryCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using ForgeFit.Domain.Primitives.Interfaces;
+
+namespace ForgeFit.Domain.Events.FoodEvents;
+
+public class DrinkEntryCreatedEvent(
+    Guid drinkEntryId, 
+    Guid userId, 
+    int volumeMl) : DomainEvent
+{
+    public Guid DrinkEntryId { get; } = drinkEntryId;
+    public Guid UserId { get; } = userId;
+    public int VolumeMl { get; } = volumeMl;
+}
