decbd08cb31be90713fe3e4904dda2f5b2ea604c
Wostan <kostyantynchyk@gmail.com>
2025-10-31 00:23:11 +0200

    Completed all repository interfaces

---
+++ src/ForgeFit.Domain/Primitives/Interfaces/Repositories/IBodyGoalRepository.cs
@@ -0,0 +1,8 @@
+﻿using ForgeFit.Domain.Aggregates.GoalAggregate;
+
+namespace ForgeFit.Domain.Primitives.Interfaces.Repositories;
+
+public interface IBodyGoalRepository : IRepository<BodyGoal>
+{
+    Task<BodyGoal?> GetByUserIdAsync(Guid userId);
+}
