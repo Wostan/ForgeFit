56cb6bc46b37b70aca810bd5c5392416abde865b
Wostan <kostyantynchyk@gmail.com>
2025-11-04 00:25:35 +0200

    Created IWorkoutGoalGenerationService and INutritionCalculationService

---
+++ src/ForgeFit.Domain/Primitives/Interfaces/Services/INutritionCalculationService.cs
@@ -0,0 +1,9 @@
+﻿using ForgeFit.Domain.Aggregates.GoalAggregate;
+using ForgeFit.Domain.ValueObjects.UserValueObjects;
+
+namespace ForgeFit.Domain.Primitives.Interfaces.Services;
+
+public interface INutritionCalculationService
+{
+    NutritionGoal CalculateNutritionGoal(UserProfile userProfile, BodyGoal bodyGoal, WorkoutGoal? workoutGoal);
+}
---
+++ src/ForgeFit.Domain/Primitives/Interfaces/Services/IWorkoutGoalGenerationService.cs
@@ -0,0 +1,9 @@
+﻿using ForgeFit.Domain.Aggregates.GoalAggregate;
+using ForgeFit.Domain.ValueObjects.UserValueObjects;
+
+namespace ForgeFit.Domain.Primitives.Interfaces.Services;
+
+public interface IWorkoutGoalGenerationService
+{
+    WorkoutGoal GenerateWorkoutGoal(UserProfile userProfile, BodyGoal bodyGoal);
+}
