30cf9b94b35bcd8ae1d99a899978a0f0475244bb
Wostan <kostyantynchyk@gmail.com>
2025-11-12 23:56:19 +0200

    Created UserController.cs with all related abstractions/classes

---
+++ src/ForgeFit.Api/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Claims;
+using ForgeFit.Application.Common.Exceptions;
+using ForgeFit.Application.Common.Interfaces.Services;
+using ForgeFit.Application.DTOs.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForgeFit.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(IUserService userService) : ControllerBase
+{
+    [Authorize]
+    [HttpGet("get-profile")]
+    [ProducesResponseType(typeof(UserProfileDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> GetProfile()
+    {
+        try
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            var profile = await userService.GetProfileByIdAsync(userId);
+            return Ok(profile);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+    
+    [Authorize]
+    [HttpPut("update-profile")]
+    [ProducesResponseType(typeof(UserProfileDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> UpdateProfile([FromBody] UserProfileDto profile)
+    {
+        try
+        { 
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            var updatedProfile = await userService.UpdateProfileByIdAsync(userId, profile);
+            return Ok(updatedProfile);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+    
+    [Authorize]
+    [HttpPut("change-password")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequest password)
+    {
+        try
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            await userService.ChangePasswordByIdAsync(userId, password);
+            return Ok("Password changed.");
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+}
---
+++ src/ForgeFit.Application/DTOs/User/UserProfileDto.cs
@@ -0,0 +1,13 @@
+﻿using ForgeFit.Domain.Enums.ProfileEnums;
+
+namespace ForgeFit.Application.DTOs.User;
+
+public record UserProfileDto(
+    string Username,
+    string? AvatarUrl,
+    DateTime DateOfBirth,
+    Gender Gender,
+    double Weight,
+    string WeightUnit,
+    double Height,
+    string HeightUnit);
