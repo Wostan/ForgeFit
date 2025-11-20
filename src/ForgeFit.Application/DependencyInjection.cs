0587476afbdffb49c78688864c7bb9d96075e7e6
Wostan <kostyantynchyk@gmail.com>
2025-11-20 21:27:18 +0200

    feat(application/mapping): Integrate Mapster DI setup (mapping)
    Adds a new DependencyInjection.cs file to configure Mapster services
    within the application layer using dependency injection.
    
    Updates Mapster to version 9.0.0-pre01 and introduces the
    Mapster.DependencyInjection package for seamless integration.
    The Common/Mappings folder has been removed, as the new DI setup
    streamlines mapping registration.

---
+++ src/ForgeFit.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ForgeFit.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMapster();
+
+        return services;
+    }
+}
